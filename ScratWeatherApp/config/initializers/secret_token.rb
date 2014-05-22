8343cefa8226f3f459bac8240e6a3dd46a90eb2d
Vu Truong <truongquangvu1@gmail.com>
2014-05-22 18:25:29 +0700

    first commit

---
+++ ScratWeatherApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScratWeatherApp::Application.config.secret_token = '38811977be8166c5491f1785115ed1518c049de71604f4b7e9644d701ad4f46ff0f97bf7cb32582414e3a5b0099d194f186162fe075524275822ae7fa8bbb9bc'
